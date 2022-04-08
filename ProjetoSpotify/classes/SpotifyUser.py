9d6be27ca4e626a0d2d0f71008ee77b6bee13e73
Yas190 <ypires656@gmail.com>
2022-04-08 18:30:41 -0300

    updated

--- ProjetoSpotify/classes/SpotifyUser.py
+++ ProjetoSpotify/classes/SpotifyUser.py
@@ -1,17 +1,17 @@
-import spotipy
-from spotipy.oauth2 import SpotifyOAuth
-import pandas as pd
-from google.cloud import bigquery
+import spotipy # importando a biblioteca para acesso a API do Spotify
+from spotipy.oauth2 import SpotifyOAuth # importando a biblioteca para autenticação no acesso a API
+import pandas as pd # biblioteca pandas para a manipulação de dados
+from google.cloud import bigquery # importando biblioteca para udo de dados do BigQuery
 
-class SpotifyUser:
+class SpotifyUser: # criando a classe SpotifyUser com atributos necessários
 
     def __init__(self, client_id, client_secret, redirect_uri, scope):
-        self.client_id = client_id
+        self.client_id = client_id # autenticação
         self.client_secret = client_secret
         self.redirect_uri = redirect_uri
-        self.scope = scope
+        self.scope = scope # o que será buscado
 
-    def get_client_id(self):
+    def get_client_id(self): # métodos get
         return self.client_id
 
     def get_client_secret(self):
@@ -23,7 +23,7 @@ class SpotifyUser:
     def get_scope(self):
         return self.scope
 
-    def set_client_id(self, client_id, inplace=False):
+    def set_client_id(self, client_id, inplace=False): # métodos set
         if inplace:
             self.client_id = client_id
         else:
@@ -47,16 +47,20 @@ class SpotifyUser:
         else:
             return SpotifyUser(self.client_id, self.client_secret, self.redirect_uri, scope)
 
-    def _top_tracks(self):
+    def _top_tracks(self): # método estático para coletar dados das músicas mais ouvidas
+        # variável para autenticação no acesso a API usando informações fornecidas no arquivo main.py
         sp = spotipy.Spotify(
             auth_manager=SpotifyOAuth(self.client_id, self.client_secret, self.redirect_uri, self.scope))
+        # variável com lista dos períodos de tempo relacionados aos dados desejados
         ranges = ['short_term', 'medium_term', 'long_term']
-
+        # criação do DataFrame vazio
         df_top_tracks = pd.DataFrame(columns=['POSICAO', 'MUSICA', 'ARTISTAS', 'DURAÇAO_EM_MS', 'PERIODO'])
-
+        # loop para a coleta dos dados
         for sp_range in ranges:
+            # método do spotipy, especificando o período e o limite de músicas)
             results = sp.current_user_top_tracks(time_range=sp_range, limit=50)
-            for i, item in enumerate(results['items']):
+            for i, item in enumerate(results['items']): # buscando as músicas na lista 'items' contando com a posição
+                # inserindo os dados do loop em um novo DataFrame
                 track = pd.DataFrame({
                     'POSICAO': i,
                     'MUSICA': item['name'],
@@ -64,12 +68,14 @@ class SpotifyUser:
                     'ARTISTAS': [artista['name'] for artista in item['artists']],
                     'PERIODO': sp_range
                 })
-
+                # concatenando DataFrame vazio com novo DataFrame
                 df_top_tracks = pd.concat([df_top_tracks, track], ignore_index=True)
 
         print(df_top_tracks.to_string())
+        # fase de envio do DataFrame para o BiqQuery. Construindo um objeto cliente
         client = bigquery.Client()
         job_config = bigquery.LoadJobConfig(
+            # estabelecendo esquema parcial da tabela, inserindo os tipos de dados das colunas
             schema=[
                 bigquery.SchemaField('POSICAO', bigquery.enums.SqlTypeNames.INT64),
                 bigquery.SchemaField('MUSICA', bigquery.enums.SqlTypeNames.STRING),
@@ -77,14 +83,17 @@ class SpotifyUser:
                 bigquery.SchemaField('ARTISTAS', bigquery.enums.SqlTypeNames.STRING),
                 bigquery.SchemaField('PERIODO', bigquery.enums.SqlTypeNames.STRING),
             ],
+            # BigQuery substitui os dados da tabela já existente usando o esquema
             write_disposition="WRITE_TRUNCATE",
         )
+        # API request
         job = client.load_table_from_dataframe(df_top_tracks, 'dynamic-market-346321.spotify.yasmin_cezere_pires',
                                                job_config=job_config
                                                )
         job.result()
+        # API request
         table = client.get_table('dynamic-market-346321.spotify.yasmin_cezere_pires')
         print( f"Loaded {table.num_rows} rows and {len(table.schema)} columns to {'dynamic-market-346321.spotify.yasmin_cezere_pires'}")
-
+    # chamando método estático
     def get_top_tracks(self):
         print(self._top_tracks())
